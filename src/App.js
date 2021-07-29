2bb9bf4869bfb4e79a2df582d77fcc4b66556e2c
Gabriel Mauas <gabymauas02@gmail.com>
2021-07-29 17:12:44 -0300

    added responsive text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VStack, Heading, IconButton, Text, Flex, HStack, Input, Button, useColorMode } from '@chakra-ui/react';
+import { VStack, Heading, IconButton, Text, Flex, HStack, Input, Button, useColorMode, Spacer } from '@chakra-ui/react';
 import { FaSun, FaMoon } from 'react-icons/fa';
 // import useColorMode from '@chakra-ui/react';
 
@@ -15,12 +15,12 @@ function App() {
           <Heading>Dark Mode Challenge</Heading>
           <IconButton bg="white" icon={ colorMode === 'light' ? <FaSun /> : <FaMoon /> } color="black" onClick={toggleColorMode} />
         </Flex>
-        <Flex direction={["row", "column"]}>
-            <Text mb="4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales turpis pulvinar sapien ultricies, sed suscipit lectus luctus. Donec commodo mollis orci, ut ultrices nunc commodo nec. Mauris feugiat auctor tellus, lobortis fringilla purus fermentum ut. Nulla aliquam blandit dolor et lobortis. Ut vehicula, risus sed eleifend ullamcorper, dolor leo dapibus turpis, sed luctus mauris sapien a tortor. Duis pulvinar, justo quis dictum accumsan, sem leo maximus metus, vel aliquet nibh nibh at nulla. Cras et libero pharetra, pretium mi nec, rhoncus nisi. Nulla ut leo ac elit pharetra interdum in quis elit. Morbi ultricies, justo at posuere viverra, nibh mauris porttitor diam, ac sollicitudin mi nisl sed ante. Sed vestibulum vehicula purus vitae sagittis. Nulla interdum pellentesque elit, ut blandit urna maximus in.</Text>
-            
-            <Text>Nam est dolor, vehicula in risus vel, fringilla vehicula lacus. Curabitur feugiat tempor consectetur. Aliquam ut euismod sem. Aenean laoreet maximus ex sed ullamcorper. Vestibulum nec aliquet orci. Praesent consequat rutrum mi non viverra. Ut in porttitor libero. Vivamus fringilla est et leo porttitor gravida. Fusce lacinia ut dui eget condimentum. Nulla vitae posuere eros, sed porttitor libero. Etiam purus est, mattis non libero finibus, facilisis luctus elit. Aliquam erat volutpat.</Text>
+        <Flex direction={["column", "row"]}>
+            <Text mb="4" mx="2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales turpis pulvinar sapien ultricies, sed suscipit lectus luctus. Donec commodo mollis orci, ut ultrices nunc commodo nec. Mauris feugiat auctor tellus, lobortis fringilla purus fermentum ut. Nulla aliquam blandit dolor et lobortis. Ut vehicula, risus sed eleifend ullamcorper, dolor leo dapibus turpis, sed luctus mauris sapien a tortor. Duis pulvinar, justo quis dictum accumsan, sem leo maximus metus, vel aliquet.</Text>
+            {/* <Spacer /> */}
+            <Text mx="2">Nam est dolor, vehicula in risus vel, fringilla vehicula lacus. Curabitur feugiat tempor consectetur. Aliquam ut euismod sem. Aenean laoreet maximus ex sed ullamcorper. Vestibulum nec aliquet orci. Praesent consequat rutrum mi non viverra. Ut in porttitor libero. Vivamus fringilla est et leo porttitor gravida. Fusce lacinia ut dui eget condimentum. Nulla vitae posuere eros, sed porttitor libero. Etiam purus est, mattis non libero finibus, facilisis luctus elit. Aliquam erat volutpat.</Text>
         </Flex>
-        <Flex direction="column" w="80%">
+        <Flex direction="column" w={["95%", "75%"]}>
           <Input type="text" variant="filled" my="4" bg="gray.200"/>
           <Input type="email" variant="filled" bg="gray.200"/>
           <HStack mt="4" justify="flex-end" spacing="10">
